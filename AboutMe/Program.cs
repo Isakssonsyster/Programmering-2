7f5e0b3ba9fd6c7af877dde803401445650caa87
Isakssonsyster <54797172+Isakssonsyster@users.noreply.github.com>
2020-08-27 11:13:29 +0200

    Create Program.cs

---
+++ AboutMe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AboutMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            String name = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("\nHow old are you?");
+            int age = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("\nAre you alive?");
+            String alive = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("\nWhat is your Name?\n" + name);
+            Console.WriteLine("\nHow old are you?\n" + age);
+            Console.WriteLine("\nAre you alive?\n" + alive);
+        }
+    }
+}
